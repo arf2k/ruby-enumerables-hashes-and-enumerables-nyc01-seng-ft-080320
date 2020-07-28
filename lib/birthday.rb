b18e480ca70cd12e08b81ea70b792f01f9d5cf8e
Alex Foreman <foreman.alexander@gmail.com>
2020-07-28 18:47:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,42 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kid, age|
-    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def happy_birthday(birthday_kids)
-#   birthday_kids.each do |name, age|
-#     puts "Happy Birthday #{name}! You are now #{age} years old!"
-#   end
-# end
